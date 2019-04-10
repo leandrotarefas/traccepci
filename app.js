371375af9d360a5da30597c121f09c64c906ff67
leandrotarefas <conta.tarefas@gmail.com>
2019-04-10 13:44:42 -0300

    save local function

--- app.js
+++ app.js
@@ -2,26 +2,34 @@
 const tlog = require("traccelog")
 const maskkcard = require("maskkcard")
 
-module.exports = function(path) {
+module.exports = function(path, saveAtLocal) {
 
     function logInfoRequest(id, content, keysForMasking) {
         const maskedContent =  maskContent(content, keysForMasking);
         tlog.logPathRequest(path,id, maskedContent);
+        
+        if(saveAtLocal){ tlog.logInfoRequest(id, maskedContent)}
     }
 
     function logInfoResponse(id, content, keysForMasking) {
         const maskedContent =  maskContent(content, keysForMasking);
         tlog.logPathResponse(path,id, maskedContent);
+
+        if(saveAtLocal){ tlog.logInfoResponse(id, maskedContent)}
     }
 
     function logInfoWrite(id, content, keysForMasking) {
         const maskedContent =  maskContent(content, keysForMasking);
         tlog.logPathWrite(path,id, maskedContent);
+
+        if(saveAtLocal){ tlog.logInfoWrite(id, maskedContent)}
     }
 
     function logInfoError(id, content, keysForMasking) {
         const maskedContent =  maskContent(content, keysForMasking);
         tlog.logPathError(path,id, maskedContent);
+
+        if(saveAtLocal){ tlog.logInfoError(id, maskedContent)}
     }
 
     function maskContent(content, keysForMasking){
